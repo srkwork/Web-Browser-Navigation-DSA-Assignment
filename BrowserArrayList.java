c464f58fc5300018f6d6d5f58b5e6f2830f63a11
Saaketh <113314505+srkwork@users.noreply.github.com>
2026-02-12 17:20:19 -0600

    Initial commit

---
+++ BrowserArrayList.java
@@ -0,0 +1,92 @@
+import java.util.*;
+
+public class BrowserArrayList<T> implements Iterable<T>{
+
+    private T[] items;
+    private int size;
+    private int front;
+    private int rear;
+    private int capacity;
+
+    public BrowserArrayList(){
+        size = 0;
+        front = 0;
+        capacity = 10;
+        items = (T[]) new Object[capacity];
+    }
+
+    public int getSize(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void clear(){
+        size = 0;
+        front = 0; 
+        rear = 0;
+    }
+
+    // Adding item to the queue (Enqueue)
+    public boolean add(T data){
+        if(size == 0) return false;
+
+        if(size == capacity){
+            T[] newItems = (T[]) new Object[capacity * 2];
+            for(int i = 0; i < size; i++){
+                newItems[i] = items[(front + i) % capacity]; // Circular copying
+            }
+            items = newItems;
+            capacity = newItems.length;
+            front = 0; 
+            rear = size;
+        }
+        items[rear] = data;
+        rear = (rear+1) % capacity; // Circular increment
+        size++;
+        return true;
+    }
+
+    // Removing item from the queue (Dequeue)
+    public T remove(){
+        if(size == 0) return null;
+
+        T it = items[front];
+        front = (front+1) % capacity;
+        size--;
+        return it;
+    }
+
+
+    // Peek at the front value
+    public T peek(){
+        if(size == 0) return null;
+        return items[front];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new BrowserArrayListIterator();
+    }
+
+    private class BrowserArrayListIterator implements Iterator<T>{
+        private int current = 0;
+
+        @Override
+        public boolean hasNext() {
+            return current < size;
+        }
+
+        @Override
+        public T next() {
+            if(!hasNext()) throw new NoSuchElementException();
+            int index = (front + current) % capacity;
+            current++;
+            return items[index];
+
+        }
+        
+    }
+    
+}
---
+++ BrowserLinkedList.java
@@ -0,0 +1,96 @@
+import java.util.*;
+
+public class BrowserLinkedList<T> implements Iterable<T> {
+    private int size;
+    private Node head;
+    private Node tail;
+
+    public BrowserLinkedList(){
+        size = 0;
+        head = null;
+        tail = null;
+    }
+
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    
+    private class Node{
+        
+        T data;
+        Node prev;
+        Node next;
+
+        Node(T data, Node prev, Node next){
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    // Adding node to the stack (at head)
+    public void addFirst(T data){
+        Node newNode = new Node(data, null, head);
+        if(head != null){
+            // Adding node to the stack (newNode = head)
+            head.prev = newNode;
+        }
+        else{
+            // List is empty therefore tail becomes newNode
+            tail = newNode;
+        }
+        head = newNode;
+        size++; 
+    }
+
+    // Removing node from the stack (from head)
+    public T removeFirst(){
+        if(isEmpty()) throw new NoSuchElementException("List is empty");
+
+        T removedHead = head.data;
+        head = head.next;
+
+        if(head != null){
+            // Removing node from the stack
+            head.prev = null;
+        }
+        else{
+            // List is empty therefore tail is empty;
+            tail = null;
+        }
+
+        size--; 
+        return removedHead;
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return new BrowserLinkedListIterator();
+    }
+    
+    private class BrowserLinkedListIterator implements Iterator<T>{
+        private Node current = head;
+        
+        @Override
+        public boolean hasNext(){
+            return current != null;
+        }
+
+        @Override
+        public T next(){
+            if(!hasNext()) throw new NoSuchElementException();
+            
+            T val = current.data;
+            current = current.next;
+            return val;
+        }
+        
+    }
+    
+}
