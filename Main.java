608f05d21497306eb63fdc99b086941293361983
Saaketh <113314505+srkwork@users.noreply.github.com>
2026-02-16 17:56:00 -0600

    Wrote some code to test current classes to make sure everything is working as expected.

--- Main.java
+++ Main.java
@@ -1,5 +1,30 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // =========== Test BrowserStack =========
+        System.out.println("======Testing BrowserStack=====");
+        BrowserStack<String> stack = new BrowserStack<>();
+
+        System.out.println("Stack is empty: " + stack.isEmpty());
+
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
+
+        System.out.println("Stack is empty: " + stack.isEmpty());
+        System.out.println("stack.peek() should be C: " + stack.peek() + '\n');
+
+        System.out.println("Iterating stack: ");
+        for(String s : stack){
+            System.out.print(s + " ");
+        }
+        System.out.println('\n');
+
+
+        System.out.println("stack.pop() should be C: " + stack.pop());
+        System.out.println("stack.pop() should be B: " + stack.pop());
+        System.out.println("stack.peek() should be A: " + stack.peek());
+
+        stack.clear(); // Clearing the whole stack
+        System.out.println("After clear, stack.isEmpty() should be true: " + stack.isEmpty());
     }
 }
