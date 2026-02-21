4529a5b4508f29302560db4a6323f2a28ea3324a
Saaketh <113314505+srkwork@users.noreply.github.com>
2026-02-21 16:19:52 -0600

    Added comments in the StackIterator.java

--- StackIterator.java
+++ StackIterator.java
@@ -2,17 +2,24 @@ import java.util.*;
 
 public class StackIterator<T> implements Iterator<T>{
 
+    // Creating an iterator used to traverse stack elements
     private Iterator<T> it;
     
+    // Stack whose whose elements will be iterated
     public StackIterator(BrowserStack<T> stack){
+
+        // Uses the stack's iterator to traverse elements
         it = stack.iterator();
     }
 
+
+    // Returns true if there are more elements in the stack.
     @Override
     public boolean hasNext() {
         return it.hasNext();
     }
 
+    // Returns the next element in the stack iteration.
     @Override
     public T next() {
         return it.next();
